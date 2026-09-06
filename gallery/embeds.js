265d9799b9331bc2cb98de311cd110016a2f4b84
jakeloftis <49935523+jakeloftis@users.noreply.github.com>
2026-09-06 11:01:04 -0500

    Update embeds.js

--- gallery/embeds.js
+++ gallery/embeds.js
@@ -6,8 +6,19 @@
 
 const imgurUrls = [
   "https://imgur.com/a/hdJ15Ui",
-  "https://imgur.com/a/bullshit-AqPh7dF",
-  
+  "https://imgur.com/a/tagilla-hip-fire-aUQmo9u",
+  "https://imgur.com/a/escape-from-duckov-lRSFo8U",
+  "https://imgur.com/a/some-people-say-hes-still-running-to-this-very-day-zR70TGR",
+  "https://imgur.com/a/i-got-kill-9EmVKGP",
+  "https://imgur.com/a/P0EWgou",
+  "https://imgur.com/a/hatchwiggle-Y2bkTiK",
+  "https://imgur.com/a/double-kill-u6crRb4",
+  "https://imgur.com/a/kfc-sane-wSwLxO1",
+  "https://imgur.com/a/ultrasonic-sexual-communication-GabGJWz",
+  "https://imgur.com/a/well-T084tYq",
+  "https://imgur.com/a/get-zone-YgKHeiW,"
+  "https://imgur.com/a/t-what-s-sole-purpose-this-army-ZowjzPW",
+  "https://imgur.com/a/hate-you-sane-qiNhoCk",
 
   // Add more below, e.g.:
   // "https://imgur.com/a/another-example-xxxxxxx",
