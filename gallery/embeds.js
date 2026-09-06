9de6a353fcaa07b5673f94366650a61dd7a6015b
jakeloftis <49935523+jakeloftis@users.noreply.github.com>
2026-09-06 10:43:01 -0500

    Create embeds.js

---
+++ gallery/embeds.js
@@ -0,0 +1,10 @@
+// Add new Imgur links here, one per line, then commit + push.
+// Works with album links (imgur.com/a/...) and gallery links (imgur.com/gallery/...).
+// The page will automatically turn each of these into an embed.
+
+const imgurUrls = [
+  "https://imgur.com/a/bullshit-AqPh7dF",
+
+  // Add more below, e.g.:
+  // "https://imgur.com/a/another-example-xxxxxxx",
+];
