0896ac81476dfe3b24988acbb5e2722aa0058172
jakeloftis <49935523+jakeloftis@users.noreply.github.com>
2026-09-14 16:32:36 -0500

    Update embeds.js

--- gallery/embeds.js
+++ gallery/embeds.js
@@ -1,25 +1,23 @@
-// Add new Imgur links here, one per line, then commit + push.
-// Works with album links (imgur.com/a/...) and gallery links (imgur.com/gallery/...).
-// The page will automatically turn each of these into an embed.
-
-
-
-const imgurUrls = [
-  "https://imgur.com/a/hdJ15Ui",
-  "https://imgur.com/a/tagilla-hip-fire-aUQmo9u",
-  "https://imgur.com/a/escape-from-duckov-lRSFo8U",
-  "https://imgur.com/a/some-people-say-hes-still-running-to-this-very-day-zR70TGR",
-  "https://imgur.com/a/i-got-kill-9EmVKGP",
-  "https://imgur.com/a/P0EWgou",
-  "https://imgur.com/a/hatchwiggle-Y2bkTiK",
-  "https://imgur.com/a/double-kill-u6crRb4",
-  "https://imgur.com/a/kfc-sane-wSwLxO1",
-  "https://imgur.com/a/ultrasonic-sexual-communication-GabGJWz",
-  "https://imgur.com/a/well-T084tYq",
-  "https://imgur.com/a/get-zone-YgKHeiW",
-  "https://imgur.com/a/t-what-s-sole-purpose-this-army-ZowjzPW",
-  "https://imgur.com/a/hate-you-sane-qiNhoCk",
+// Add new gallery items here, then commit + push.
+// url: the direct i.imgur.com file (right-click the image/video on the
+//      imgur page -> "Copy image address" / "Copy video address")
+// caption: optional text shown under the media, or null for none
+// type: 'image' or 'video' (mp4) - must match the actual file
+const galleryItems = [
+  { url: "https://i.imgur.com/JZsuEnNl.jpg", caption: null, type: "image" },
+  { url: "https://i.imgur.com/ZGddXQK.mp4", caption: "Tagilla Hip Fire", type: "video" },
+  { url: "https://i.imgur.com/fB70gn7.mp4", caption: "Escape From Duckov", type: "video" },
+  { url: "https://i.imgur.com/RIQYEec.mp4", caption: "Some people say he's still running to this very day", type: "video" },
+  { url: "https://i.imgur.com/aTmFSkJ.mp4", caption: "I GOT A KILL", type: "video" },
+  { url: "https://i.imgur.com/XAIBkVm.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/YkkjphR.mp4", caption: "HatchWiggle", type: "video" },
+  { url: "https://i.imgur.com/PPu4DAR.mp4", caption: "DOUBLE KILL", type: "video" },
+  { url: "https://i.imgur.com/GkVlz9F.mp4", caption: "KFC Sane", type: "video" },
+  { url: "https://i.imgur.com/OtMqd3N.mp4", caption: "Ultrasonic Sexual Communication", type: "video" },
+  { url: "https://i.imgur.com/I3lwKD5.mp4", caption: "WELL", type: "video" },
+  { url: "https://i.imgur.com/GvG2NO5.mp4", caption: "Get in the zone!", type: "video" },
+  { url: "https://i.imgur.com/yPA4aX4.mp4", caption: "T! What's your sole purpose in this army!?", type: "video" },
 
   // Add more below, e.g.:
-  // "https://imgur.com/a/another-example-xxxxxxx",
+  // { url: "https://i.imgur.com/xxxxxxx.jpg", caption: "My caption", type: "image" },
 ];
