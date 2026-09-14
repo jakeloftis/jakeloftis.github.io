f5abe9adeae901a052179c7b7f49612a1d0a493a
jakeloftis <49935523+jakeloftis@users.noreply.github.com>
2026-09-14 16:34:46 -0500

    Update embeds.js

--- gallery/embeds.js
+++ gallery/embeds.js
@@ -5,19 +5,19 @@
 // type: 'image' or 'video' (mp4) - must match the actual file
 const galleryItems = [
   { url: "https://i.imgur.com/JZsuEnNl.jpg", caption: null, type: "image" },
-  { url: "https://i.imgur.com/ZGddXQK.mp4", caption: "Tagilla Hip Fire", type: "video" },
-  { url: "https://i.imgur.com/fB70gn7.mp4", caption: "Escape From Duckov", type: "video" },
-  { url: "https://i.imgur.com/RIQYEec.mp4", caption: "Some people say he's still running to this very day", type: "video" },
-  { url: "https://i.imgur.com/aTmFSkJ.mp4", caption: "I GOT A KILL", type: "video" },
+  { url: "https://i.imgur.com/ZGddXQK.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/fB70gn7.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/RIQYEec.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/aTmFSkJ.mp4", caption: null, type: "video" },
   { url: "https://i.imgur.com/XAIBkVm.mp4", caption: null, type: "video" },
-  { url: "https://i.imgur.com/YkkjphR.mp4", caption: "HatchWiggle", type: "video" },
-  { url: "https://i.imgur.com/PPu4DAR.mp4", caption: "DOUBLE KILL", type: "video" },
-  { url: "https://i.imgur.com/GkVlz9F.mp4", caption: "KFC Sane", type: "video" },
-  { url: "https://i.imgur.com/OtMqd3N.mp4", caption: "Ultrasonic Sexual Communication", type: "video" },
-  { url: "https://i.imgur.com/I3lwKD5.mp4", caption: "WELL", type: "video" },
-  { url: "https://i.imgur.com/GvG2NO5.mp4", caption: "Get in the zone!", type: "video" },
-  { url: "https://i.imgur.com/yPA4aX4.mp4", caption: "T! What's your sole purpose in this army!?", type: "video" },
+  { url: "https://i.imgur.com/YkkjphR.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/PPu4DAR.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/GkVlz9F.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/OtMqd3N.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/I3lwKD5.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/GvG2NO5.mp4", caption: null, type: "video" },
+  { url: "https://i.imgur.com/yPA4aX4.mp4", caption: null, type: "video" },
 
   // Add more below, e.g.:
-  // { url: "https://i.imgur.com/xxxxxxx.jpg", caption: "My caption", type: "image" },
+  // { url: "https://i.imgur.com/xxxxxxx.jpg", caption: null, type: "image" },
 ];
